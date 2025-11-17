d5d4daf7408596f793193cc725f1127deee2d07b
Pablo <pgm00062@red.ujaen.es>
2025-11-17 11:48:34 +0100

    feat: Update CORS configuration to include additional allowed origins

--- src/main/java/freelancer/backendtfg/shared/CorsConfig.java
+++ src/main/java/freelancer/backendtfg/shared/CorsConfig.java
@@ -16,7 +16,9 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",  // frontend Next.js
-                        "http://127.0.0.1:3000"
+                        "http://127.0.0.1:3000",
+                        "https://fronted-tuis5q8cd-pablos-projects-a30504fc.vercel.app",
+                        "https://fronted-tfg.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
@@ -29,6 +31,8 @@ public class CorsConfig implements WebMvcConfigurer {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedOrigin("http://127.0.0.1:3000");
+        configuration.addAllowedOrigin("https://fronted-tuis5q8cd-pablos-projects-a30504fc.vercel.app");
+        configuration.addAllowedOrigin("https://fronted-tfg.vercel.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
